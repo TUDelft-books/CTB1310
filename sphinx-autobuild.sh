2b83e03338bd394e4fc75f63b676c39f35cc8b8f
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-01-02 13:16:41 +0100

    Update sphinx-autobuild.sh

--- sphinx-autobuild.sh
+++ sphinx-autobuild.sh
@@ -1,5 +1,7 @@
-conda init
-conda activate CTB1310
-pip install -r requirements.txt
+conda deactivate 2>/dev/null || true
+unset CONDA_DEFAULT_ENV CONDA_PREFIX CONDA_PROMPT_MODIFIER 2>/dev/null || true
+python -m venv venv
+source venv/Scripts/activate
+pip install -r requirements.txt sphinx-autobuild
 jupyter-book config sphinx book/
 sphinx-autobuild book book/_build/html --open-browser
