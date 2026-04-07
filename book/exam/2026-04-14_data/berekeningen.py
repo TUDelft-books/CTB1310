eb52bf0e26e550a7b569293d0a6db5d1afdacb44
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2026-04-07 16:16:30 +0200

    finalized exam

--- book/exam/2026-04-14_data/berekeningen.py
+++ book/exam/2026-04-14_data/berekeningen.py
@@ -15,8 +15,10 @@ L = sym.Integer(2000)
 b_2 = h / 2 * 3 / 4
 print('b_2 =', b_2,'approx', b_2.evalf())
 
-Sy = b * t * b / 2 + h/2 / 4 * 5 * t * (b - b_2 / 2) * 2
+Sy = 2 * b * t * b / 2 + h/2 / 4 * 5 * t * (b - b_2 / 2) * 2
+print('Sy =', Sy)
 A = b * t * 2 + h / 2 / 4 * 5 * t * 2 + h * t
+print('A =', A)
 
 zy = Sy / A
 
@@ -27,9 +29,10 @@ print('Izz =', Izz)
 Izz_2 = Izz - b * t **3 /12 * 2
 print('Izz_2 =', Izz_2)
 
-Iyy = h * t **3 / 12 + h * t * zy **2 + t * b **3 / 12 * 2 + t * b * (zy - b/2)**2 * 2 + t / 3 * 5 * b_2 **3 * 2 + t * h / 2 / 4 * 5 * (b - b_2/2 - zy)**2 * 2
+Iyy = h * t **3 / 12 + h * t * zy **2 + t * b **3 / 12 * 2 + t * b * (zy - b/2)**2 * 2 + t / 3 * 5 * b_2 **3 * 2 / 12 + t * h / 2 / 4 * 5 * (b - b_2/2 - zy)**2 * 2
 print('Iyy =', Iyy)
 Iyy_2 = Iyy - h * t **3 / 12
+print('Iyy_2 =', Iyy_2)
 
 Sy = h * t * zy + zy * t * zy/2 * 2
 print('Sy =', Sy)
@@ -42,6 +45,7 @@ tau = V * Sy / (Iyy * t * 2)
 print('tau =', tau, 'approx', tau.evalf())
 
 A_m = b * h - h * b_2 / 2
+print('A_m =', A_m)
 
 tau_2 = T / 2 / A_m / t
 
